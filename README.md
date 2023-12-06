139e8b9df146600970b134e8efc4b755c58488ce
artdian <46709199+jartdian@users.noreply.github.com>
2023-12-06 22:46:02 +0100

    switch to cdns, edit folder structure, reverse bar axes, add cityName label, fix decoding for c++ and c#

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-###A website that displays information of in-demand programming languages for job searches in the Netherlands.
+### A website that displays information of in-demand programming languages for job searches in the Netherlands.
 
---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let map = L.map("map").setView([51.9225, 4.47917], 10);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors",
+  }).addTo(map);
+
+  fetch("https://dutch-dev-trends-backend.vercel.app/languages")
+    .then((response) => response.json())
+    .then((citiesData) => {
+      console.log("Received data:", citiesData); // Log the received data
+
+      // Loop through the cities and add markers
+      Object.keys(citiesData).forEach((cityName) => {
+        let city = citiesData[cityName];
+        let marker = L.marker(city.coordinates).addTo(map);
+
+        console.log("Chart data:", city.data);
+
+        // Create a popup with a horizontal bar chart
+        marker.bindPopup(createChartPopup(cityName, city.data));
+      });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+
+  // Function to create a Chart.js popup
+  function createChartPopup(cityName, data) {
+    let popupContent = document.createElement("div");
+    popupContent.innerHTML =
+      '<canvas id="chart" width="100%" height="100%"></canvas>';
+
+    // Create the bar chart
+    let ctx = popupContent.querySelector("#chart").getContext("2d");
+    let languages = Object.keys(data); // languages
+    let values = Object.values(data); // values 
+    let decodedLanguages = languages.map(label => decodeURIComponent(label.toLowerCase()));
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: decodedLanguages,
+        datasets: [
+          {
+            data: values,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            label: cityName,
+          },
+        ],
+      },
+      options: {
+        indexAxis: 'y',
+        scales: {
+         y: {
+          beginAtZero: false
+         }
+        },
+      },
+    });
+
+    return popupContent;
+  }
+});
