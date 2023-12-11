74b7eda0bbc09661d2024b3f376c292c58f1ab2c
artdian <46709199+jartdian@users.noreply.github.com>
2023-12-11 19:24:42 +0100

    fix #2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let map = L.map("map").setView([52.0907, 5.1214], 10);
+  let map = L.map("map").setView([52.0907, 5.1214], 8);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap contributors",
   }).addTo(map);
@@ -11,23 +11,35 @@ document.addEventListener("DOMContentLoaded", function () {
       Object.keys(citiesData).forEach((cityName) => {
         let city = citiesData[cityName];
         let marker = L.marker(city.coordinates).addTo(map);
+        marker.on("click", function () {
+          const currentLatLng = marker.getLatLng();
 
-        console.log("Chart data:", city.data);
+          const adjustedLatLng = L.latLng(
+            currentLatLng.lat + 0.15,
+            currentLatLng.lng
+          );
+          // Set the map view to the marker's coordinates when the popup is opened
+          marker.on("popupopen", function () {
+            map.setView(adjustedLatLng, map.getZoom(), {
+              animate: true,
+              duration: 1,
+            });
+          });
+        });
 
-        // Create a popup with a horizontal bar chart
         marker.bindPopup(createChartPopup(cityName, city.data));
       });
     })
     .catch((error) => console.error("Error fetching data:", error));
 
+  // Create a Chartjs popup
   function createChartPopup(cityName, data) {
     let popupContent = document.createElement("div");
     popupContent.innerHTML =
       '<canvas id="chart" width="100%" height="100%"></canvas>';
-
     let ctx = popupContent.querySelector("#chart").getContext("2d");
-    let languages = Object.keys(data); // languages
-    let values = Object.values(data); // values
+    let languages = Object.keys(data);
+    let values = Object.values(data); 
     let decodedLanguages = languages.map((label) =>
       decodeURIComponent(label.toLowerCase())
     );
